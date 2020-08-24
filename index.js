5d8f5b9c090ae4dc2d6b4a9856cb8b18a8fb6e7d
Nóra Bozóné Krasznay <krasznaynori@gmail.com>
2020-08-24 16:21:43 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-// Create variables for the game state
-let player1Score = 0
-let player2Score = 0
-let player1Turn = true
+let player1Score = 0;
+let player2Score = 0;
+let player1Turn = true;
 
-// Create variables to store references to the necessary DOM nodes
 const player1Dice = document.getElementById("player1Dice")
 const player2Dice = document.getElementById("player2Dice")
 const player1Scoreboard = document.getElementById("player1Scoreboard")
@@ -13,67 +11,59 @@ const rollBtn = document.getElementById("rollBtn")
 const resetBtn = document.getElementById("resetBtn")
 
 function showResetBtn() {
-    rollBtn.style.display = "none"
-    resetBtn.style.display = "block"
+    resetBtn.style.display = 'block'
+    rollBtn.style.display = 'none'
 }
-    
 
-
-/* Hook up a click event listener to the Roll Dice Button. */
- rollBtn.addEventListener("click", function() {
-    const randomNumber = Math.floor(Math.random() * 6) + 1
-
-    if (player1Turn) {
-        player1Score += randomNumber
-        player1Scoreboard.textContent = player1Score
-        player1Dice.textContent = randomNumber
-        player1Dice.classList.remove("active")
-        player2Dice.classList.add("active")
-        message.textContent = "Player 2 Turn"
-    } else {
-        player2Score += randomNumber
-        player2Scoreboard.textContent = player2Score
-        player2Dice.textContent = randomNumber
-        player2Dice.classList.remove("active")
-        player1Dice.classList.add("active")
-        message.textContent = "Player 1 Turn"
-    }
-     
-    
-    // 1. Check if a player has won. If so, change the message to "Player X has won!"
-    // 2. Hide the Roll Dice Button and show the Reset Button. Hint: use display none/block
-    
-    if(player1Score >= 20) {
-        message.textContent = "Player 1 Won!"
-        showResetBtn()
-        
-        
-    }else if(player2Score >= 20) {
-        message.textContent = "Player 2 Won!"
-        showResetBtn()
-        
-         }
-    
+rollBtn.addEventListener("click", function() {
+  const randomNumber = Math.floor((Math.random() *6)+1)
+  
+  if(player1Turn){
+    player1Score += randomNumber
+    player1Scoreboard.textContent = player1Score
+    player1Dice.textContent = randomNumber
+    player1Dice.classList.remove("active")
+    player2Dice.classList.add("active")
+    message.textContent = "Player 2 Turn"
+  }else {
+    player2Score += randomNumber
+    player2Scoreboard.textContent = player2Score
+    player2Dice.textContent = randomNumber
+    player2Dice.classList.remove("active")
+    player1Dice.classList.add("active")
+    message.textContent = "Player 1 Turn"
+  }
+  if(player1Score >= 20){
+  message.textContent = "Player 1 Won!"
+  showResetBtn()
+  }
+else if(player2Score >= 20){
+  message.textContent = "Player 2 Won!"
+ showResetBtn()
+}
   player1Turn = !player1Turn
-     
- })
+})
+
+
+
+
+resetBtn.addEventListener("click", function(){
+  reset()
+})
+
+function reset(){
  
- resetBtn.addEventListener('click', function() {
-        reset()
-    })
- 
- function reset(){   
-            player1Score = 0
-            player2Score = 0 
-            player1Turn = true  
-            resetBtn.style.display = 'none'
-            rollBtn.style.display = "block"
-            player1Dice.textContent = "-"
-            player2Dice.textContent = "-"
-            player1Scoreboard.textContent = 0
-            player2Scoreboard.textContent = 0
-            message.textContent = "Player 1 Turn"
-            player2Dice.classList.remove("active")
-            player1Dice.classList.add("active")
-            }
+    player1Score = 0
+    player2Score = 0
+    player1Scoreboard.textContent = player1Score
+    player1Dice.textContent = "-"
+    player2Scoreboard.textContent = player2Score
+    player2Dice.textContent = "-"
+    player2Dice.classList.remove("active")
+    player1Dice.classList.add("active")
+    message.textContent = "Player 1 Turn"
+    player1Turn = true
+    resetBtn.style.display = "none"
+    rollBtn.style.display = "block"
+}
             
